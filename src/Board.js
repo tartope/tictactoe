24d766dcc3c4add87c00c7ecc63cdc1778d125de
Tunisia Artope <tartope@xpanxion.com>
2023-05-28 12:16:50 -0400

    Add checkWinner functionality. Begin to display winner, or tie.

--- src/Board.js
+++ src/Board.js
@@ -5,13 +5,47 @@ export const Board = () => {
     //usestate that establishes a value that determines which player's turn it is
     let [playerTurn, setPlayerTurn] = useState('X')
     //Track which items are being clicked. Default will be an empty string that will eventually be x or o.
-    const [cells, setCells] = useState(Array(9).fill(""))
-
+    const [xoInput, setXoInput] = useState(Array(9).fill(""));
+    const [winner, setWinner] = useState();
     
+    //Establish array's with the winning combinations of the game   
+    //Check if user inputs statisfy the requirements for winning a game
+    const checkWinner = (squares) => {
+        const winningCombos = {
+            across: [
+                [0, 1, 2],
+                [3, 4, 5], 
+                [6, 7, 8], 
+            ],
+            down:[
+                [0, 3, 6],
+                [1, 4, 7], 
+                [2, 5, 8], 
+            ],
+            diagonal: [
+                [0, 4, 8], [2, 4, 6] 
+            ]
+        }
+
+        for (let combo in winningCombos) {
+            winningCombos[combo].forEach((pattern) =>{
+                if (squares[pattern[0]] === '' || squares[pattern[1]] === '' || squares[pattern[2]] === '' ) {
+                    //do nothing
+                } else if (squares[pattern[0]] === squares[pattern[1]] && squares[pattern[1]] === squares[pattern[2]]) {
+                    setWinner(squares[pattern[0]]);
+                    console.log(winner)
+                }
+            });
+        }
+    };
 
     const handleClick = (num) => {
+        if (xoInput[num] !== ""){
+            return;
+        }
+
         //Create squares variable and copy array of empty cells
-        let squares = [...cells];
+        let squares = [...xoInput];
 
         if (playerTurn === 'X') {
             //Set that number square in array to X
@@ -23,57 +57,46 @@ export const Board = () => {
             setPlayerTurn('X')
         }
         //Update cells state variable to the squares array so it remembers each item that is clicked
-        setCells(squares)
-        console.log(squares);
+        checkWinner(squares);
+        setXoInput(squares)
+        // console.log(squares);
     }
-
-    //Establish array's with the winning combinations of the game
-
-    // const winningCombos = [
-    //     [0, 1, 2], [3, 4, 5], [6, 7, 8], //Rows
-    //     [0, 3, 6], [1, 4, 7], [2, 5, 8], //Columns
-    //     [0, 4, 8], [2, 4, 6] //Diagnonals 
-    // ];
-
-    // if (winningCombos[0] = ['X','X','X']) {
-    
-    // }    
-
-    //Check if user inputs statisfy the requirements for winning a game
-
-    //display to the users that one of the players has won if there inputs statisfy the requirements for winning
 
 
   return (
     <div className='container'>
-        Turn: {playerTurn}
+        Player {playerTurn}, its your turn!
         <div className='board'>
 
             <div className='board1'>
 
                 <div className="board-row">
-                    <button className="square" num={0} onClick={()=>handleClick(0)}>{cells[0]}</button>
-                    <button className="square" num={1} onClick={()=>handleClick(1)}>{cells[1]}</button>
-                    <button className="square" num={2} onClick={()=>handleClick(2)}>{cells[2]}</button>
+                    <button className="square" num={0} onClick={()=>handleClick(0)}>{xoInput[0]}</button>
+                    <button className="square" num={1} onClick={()=>handleClick(1)}>{xoInput[1]}</button>
+                    <button className="square" num={2} onClick={()=>handleClick(2)}>{xoInput[2]}</button>
                 </div>
 
                 <div className="board-row">
-                    <button className="square" num={3} onClick={()=>handleClick(3)}>{cells[3]}</button>
-                    <button className="square" num={4} onClick={()=>handleClick(4)}>{cells[4]}</button>
-                    <button className="square" num={5} onClick={()=>handleClick(5)}>{cells[5]}</button>
+                    <button className="square" num={3} onClick={()=>handleClick(3)}>{xoInput[3]}</button>
+                    <button className="square" num={4} onClick={()=>handleClick(4)}>{xoInput[4]}</button>
+                    <button className="square" num={5} onClick={()=>handleClick(5)}>{xoInput[5]}</button>
                 </div>
 
                 <div className="board-row">
-                    <button className="square" num={6} onClick={()=>handleClick(6)}>{cells[6]}</button>
-                    <button className="square" num={7} onClick={()=>handleClick(7)}>{cells[7]}</button>
-                    <button className="square" num={8} onClick={()=>handleClick(8)}>{cells[8]}</button>
+                    <button className="square" num={6} onClick={()=>handleClick(6)}>{xoInput[6]}</button>
+                    <button className="square" num={7} onClick={()=>handleClick(7)}>{xoInput[7]}</button>
+                    <button className="square" num={8} onClick={()=>handleClick(8)}>{xoInput[8]}</button>
                 </div>
 
             </div>
 
         </div>
-        
+        {/* //display to the users that one of the players has won if there inputs statisfy the requirements for winning */}
+        {/* { winner && <p>Player {winner} is the winner!</p>} */}
+        { winner === 'X' || winner === 'O' ? <p>Player {winner} is the winner!</p> : <p>Game is tied</p>}
 
     </div>
   )
 }
+
+
